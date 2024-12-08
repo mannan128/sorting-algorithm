a7d569dc3e91e58a655e68fddfe4e13bdc970a6c
pnt123st <imannanr@gmail.com>
2024-12-08 12:06:19 -0500

    selection-sort

---
+++ src/selectionsort/SelectionSort.java
@@ -0,0 +1,29 @@
+package selectionsort;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int arr[] = {6,3,8,2,9,4,1};
+
+        slecSort(arr);
+
+        for (int x : arr){
+            System.out.print(x);
+        }
+    }
+
+    private static void slecSort(int[] arr) {
+
+        for (int x=0; x<arr.length-1; x++){
+            int min = x;
+            for (int y=x+1; y<arr.length; y++){
+                if (arr[min] > arr[y]){
+                    min = y;
+                }
+            }
+            int temp = arr[x];
+            arr[x] = arr[min];
+            arr[min] = temp;
+        }
+    }
+}
